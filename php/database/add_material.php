79812eae141c61a78f739c119c4f51524bd5a92d
Rolan <89405318+Zak618@users.noreply.github.com>
2023-12-17 23:47:30 +0300

    Обновлено до v0.1s

--- php/database/add_material.php
+++ php/database/add_material.php
@@ -13,11 +13,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Добавляем текстовый материал в базу данных
         $insertTextMaterialSql = "INSERT INTO materials (lesson_id, type, content) VALUES ('$lesson_id', 'text', '$text_content')";
         $conn->query($insertTextMaterialSql);
-
     } elseif ($material_type === 'test') {
         $test_question = $_POST['testMaterialQuestion'];
         $test_options = $_POST['testMaterialOptions'];
         $correct_answers = $_POST['correctAnswers'];
+        $test_attempts = isset($_POST['testMaterialAttempts']) ? intval($_POST['testMaterialAttempts']) : 0;
+        $test_material_points = isset($_POST['testMaterialPoints']) ? intval($_POST['testMaterialPoints']) : 0;
 
         // Преобразуем индексы ответов (начиная с 1) в числа и вычитаем 1
         $correct_answers = array_map(function ($value) {
@@ -29,14 +30,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $serialized_correct_answers = implode(",", $correct_answers);
 
         // Добавляем тестовый материал в базу данных
-        $insertTestMaterialSql = "INSERT INTO materials (lesson_id, type, question, options, correct_answer) VALUES ('$lesson_id', 'test', '$test_question', '$serialized_options', '$serialized_correct_answers')";
+        $insertTestMaterialSql = "INSERT INTO materials (lesson_id, type, question, options, correct_answer, attempts, points) VALUES ('$lesson_id', 'test', '$test_question', '$serialized_options', '$serialized_correct_answers', '$test_attempts', '$test_material_points')";
         $conn->query($insertTestMaterialSql);
     }
-   
+
     // Перенаправляем пользователя обратно на страницу урока
     header("Location: ../../../diploma-project/php/lesson_details.php?lesson_id=$lesson_id");
 } else {
     // Если запрос не методом POST, возвращаем ошибку
     echo json_encode(['error' => 'Неверный метод запроса.']);
 }
-?>
--- php/lesson_details.php
+++ php/lesson_details.php
@@ -74,115 +74,120 @@ if (isset($_GET['lesson_id'])) {
                     <!-- Кнопка для добавления материала -->
                     <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#addMaterialModal">Добавить материал</button>
                 </div>
-            <!-- Модальное окно для добавления материала -->
-            <div class="modal fade" id="addMaterialModal" tabindex="-1" aria-labelledby="addMaterialModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="addMaterialModalLabel">Добавить материал</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <!-- Форма для выбора типа материала -->
-                            <form action='../php/database/add_material.php' method='post'>
-                                <input type='hidden' name='lesson_id' value='<?php echo $lesson_id; ?>'>
+                <!-- Модальное окно для добавления материала -->
+                <div class="modal fade" id="addMaterialModal" tabindex="-1" aria-labelledby="addMaterialModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="addMaterialModalLabel">Добавить материал</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <!-- Форма для выбора типа материала -->
+                                <form action='../php/database/add_material.php' method='post'>
+                                    <input type='hidden' name='lesson_id' value='<?php echo $lesson_id; ?>'>
 
-                                <div class='mb-3'>
-                                    <label for='materialType' class='form-label'>Выберите тип материала:</label>
-                                    <select class='form-select' name='materialType' id='materialType' required>
-                                        <option value='text'>Текстовая информация</option>
-                                        <option value='test'>Тестовая задача</option>
-                                    </select>
-                                </div>
-
-                                <!-- Поля для текстовой информации -->
-                                <div class='mb-3' id='textMaterialFields' style='display: none;'>
-                                    <label for='textMaterialContent' class='form-label'>Текстовое содержание:</label>
-                                    <textarea class='form-control' name='textMaterialContent'></textarea>
-                                </div>
-
-                                <div class="mb-3" id="testMaterialFields" style="display: none;">
-                                    <!-- Поля для тестовой задачи -->
-                                    <label for="testMaterialQuestion" class="form-label">Вопрос:</label>
-                                    <input type="text" class="form-control" name="testMaterialQuestion">
-
-                                    <label for="testMaterialOptions" class="form-label">Варианты ответов:</label>
-                                    <div id="checkboxContainer">
-                                        <!-- Здесь будут отображаться чекбоксы -->
-                                    </div>
-                                    <button type="button" class="btn btn-success" onclick="addCheckbox()">Добавить вариант ответа</button>
-                                    <button type="button" class="btn btn-danger" onclick="removeCheckbox()">Удалить последний вариант ответа</button>
-
-                                    <label for="testMaterialCorrectAnswers" class="form-label">Верные ответы:</label>
-                                    <div id="correctCheckboxContainer">
-                                        <!-- Здесь будут отображаться чекбоксы для выбора верных ответов -->
+                                    <div class='mb-3'>
+                                        <label for='materialType' class='form-label'>Выберите тип материала:</label>
+                                        <select class='form-select' name='materialType' id='materialType' required>
+                                            <option value='text'>Текстовая информация</option>
+                                            <option value='test'>Тестовая задача</option>
+                                        </select>
                                     </div>
 
-                                </div>
+                                    <!-- Поля для текстовой информации -->
+                                    <div class='mb-3' id='textMaterialFields' style='display: none;'>
+                                        <label for='textMaterialContent' class='form-label'>Текстовое содержание:</label>
+                                        <textarea class='form-control' name='textMaterialContent'></textarea>
+                                    </div>
 
-                                <button type='submit' class='btn btn-primary'>Добавить материал</button>
-                            </form>
+                                    <div class="mb-3" id="testMaterialFields" style="display: none;">
+                                        <!-- Поля для тестовой задачи -->
+                                        <label for="testMaterialQuestion" class="form-label">Вопрос:</label>
+                                        <input type="text" class="form-control" name="testMaterialQuestion">
+                                        <label for="testMaterialPoints" class="form-label">Количество баллов:</label>
+                                        <input type="number" class="form-control" name="testMaterialPoints" value="0" min="0">
+
+                                        <label for="testMaterialAttempts" class="form-label">Количество попыток (необязательно):</label>
+                                        <input type="number" class="form-control" name="testMaterialAttempts" min="1" max="10">
+
+                                        <label for="testMaterialOptions" class="form-label">Варианты ответов:</label>
+                                        <div id="checkboxContainer">
+                                            <!-- Здесь будут отображаться чекбоксы -->
+                                        </div>
+                                        <button type="button" class="btn btn-success" onclick="addCheckbox()">Добавить вариант ответа</button>
+                                        <button type="button" class="btn btn-danger" onclick="removeCheckbox()">Удалить последний вариант ответа</button>
+
+                                        <label for="testMaterialCorrectAnswers" class="form-label">Верные ответы:</label>
+                                        <div id="correctCheckboxContainer">
+                                            <!-- Здесь будут отображаться чекбоксы для выбора верных ответов -->
+                                        </div>
+
+                                    </div>
+
+                                    <button type='submit' class='btn btn-primary'>Добавить материал</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <script>
-                function toggleMaterialFields() {
-                    var materialType = document.getElementById('materialType').value;
-                    var textFields = document.getElementById('textMaterialFields');
-                    var testFields = document.getElementById('testMaterialFields');
+                <script>
+                    function toggleMaterialFields() {
+                        var materialType = document.getElementById('materialType').value;
+                        var textFields = document.getElementById('textMaterialFields');
+                        var testFields = document.getElementById('testMaterialFields');
 
-                    textFields.style.display = 'none';
-                    testFields.style.display = 'none';
+                        textFields.style.display = 'none';
+                        testFields.style.display = 'none';
 
-                    if (materialType === 'text') {
-                        textFields.style.display = 'block';
-                    } else if (materialType === 'test') {
-                        testFields.style.display = 'block';
+                        if (materialType === 'text') {
+                            textFields.style.display = 'block';
+                        } else if (materialType === 'test') {
+                            testFields.style.display = 'block';
+                        }
                     }
-                }
 
-                function addCheckbox() {
-                    var checkboxContainer = document.getElementById('checkboxContainer');
-                    var newCheckbox = document.createElement('div');
-                    var index = checkboxContainer.children.length + 1;
-                    newCheckbox.innerHTML = '<input type="text" class="form-control" name="testMaterialOptions[]" placeholder="Вариант ответа ' + index + '">';
-                    checkboxContainer.appendChild(newCheckbox);
+                    function addCheckbox() {
+                        var checkboxContainer = document.getElementById('checkboxContainer');
+                        var newCheckbox = document.createElement('div');
+                        var index = checkboxContainer.children.length + 1;
+                        newCheckbox.innerHTML = '<input type="text" class="form-control" name="testMaterialOptions[]" placeholder="Вариант ответа ' + index + '">';
+                        checkboxContainer.appendChild(newCheckbox);
 
-                    // Добавляем чекбокс для выбора верного ответа
-                    addCorrectCheckbox(index);
-                }
-
-                // Функция для удаления последнего чекбокса
-                function removeCheckbox() {
-                    var checkboxContainer = document.getElementById('checkboxContainer');
-                    var correctCheckboxContainer = document.getElementById('correctCheckboxContainer');
-                    if (checkboxContainer.children.length > 1) {
-                        checkboxContainer.removeChild(checkboxContainer.lastChild);
-
-                        // Удаляем соответствующий чекбокс для выбора верного ответа
-                        correctCheckboxContainer.removeChild(correctCheckboxContainer.lastChild);
+                        // Добавляем чекбокс для выбора верного ответа
+                        addCorrectCheckbox(index);
                     }
-                }
 
-                // Функция для добавления чекбокса для выбора верного ответа
-                function addCorrectCheckbox(index) {
-                    var correctCheckboxContainer = document.getElementById('correctCheckboxContainer');
-                    var newCheckbox = document.createElement('div');
-                    newCheckbox.innerHTML = '<input type="checkbox" name="correctAnswers[]" value="' + index + '"> Вариант ' + index;
-                    correctCheckboxContainer.appendChild(newCheckbox);
-                }
+                    // Функция для удаления последнего чекбокса
+                    function removeCheckbox() {
+                        var checkboxContainer = document.getElementById('checkboxContainer');
+                        var correctCheckboxContainer = document.getElementById('correctCheckboxContainer');
+                        if (checkboxContainer.children.length > 1) {
+                            checkboxContainer.removeChild(checkboxContainer.lastChild);
+
+                            // Удаляем соответствующий чекбокс для выбора верного ответа
+                            correctCheckboxContainer.removeChild(correctCheckboxContainer.lastChild);
+                        }
+                    }
+
+                    // Функция для добавления чекбокса для выбора верного ответа
+                    function addCorrectCheckbox(index) {
+                        var correctCheckboxContainer = document.getElementById('correctCheckboxContainer');
+                        var newCheckbox = document.createElement('div');
+                        newCheckbox.innerHTML = '<input type="checkbox" name="correctAnswers[]" value="' + index + '"> Вариант ' + index;
+                        correctCheckboxContainer.appendChild(newCheckbox);
+                    }
 
 
-                document.getElementById('materialType').addEventListener('change', toggleMaterialFields);
+                    document.getElementById('materialType').addEventListener('change', toggleMaterialFields);
 
 
-                toggleMaterialFields();
-            </script>
+                    toggleMaterialFields();
+                </script>
 
 
-<?php
+    <?php
         } else {
             echo "У вас нет прав для редактирования материалов урока.";
         }
@@ -194,4 +199,4 @@ if (isset($_GET['lesson_id'])) {
 }
 
 include_once "./base/footer.php";
-?>
+    ?>
